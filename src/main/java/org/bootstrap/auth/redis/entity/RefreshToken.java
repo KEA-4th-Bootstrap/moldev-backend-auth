c9593bafec5024bc83faa03c4ccb33b7fdf91849
kdhhuns2000 <52370750+kdhhuns2000@users.noreply.github.com>
2024-04-11 17:53:17 +0900

    KBB-19 [Feat] Refresh Token Entity 추가

---
+++ src/main/java/org/bootstrap/auth/redis/entity/RefreshToken.java
@@ -0,0 +1,29 @@
+package org.bootstrap.auth.redis.entity;
+
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.redis.core.RedisHash;
+
+@Getter
+@Builder
+@RedisHash(value = "refreshToken")
+public class RefreshToken {
+
+    @Id
+    private Long memberId;
+
+    private String refreshToken;
+
+    public static RefreshToken create(Long memberId, String refreshToken) {
+        return RefreshToken.builder()
+                .memberId(memberId)
+                .refreshToken(refreshToken)
+                .build();
+    }
+
+    public void update(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+}
