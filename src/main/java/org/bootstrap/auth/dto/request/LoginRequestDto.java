a79cfad7de07811cb17afa119349eeafbd9ae66c
kdhhuns2000 <52370750+kdhhuns2000@users.noreply.github.com>
2024-04-11 22:51:23 +0900

    KBB-19 [Refactor] record 타입으로 변경

--- src/main/java/org/bootstrap/auth/dto/request/LoginRequestDto.java
+++ src/main/java/org/bootstrap/auth/dto/request/LoginRequestDto.java
@@ -2,8 +2,8 @@ package org.bootstrap.auth.dto.request;
 
 import lombok.Getter;
 
-@Getter
-public class LoginRequestDto {
-    private String email;
-    private String password;
+public record LoginRequestDto (
+    String email,
+    String password
+) {
 }
