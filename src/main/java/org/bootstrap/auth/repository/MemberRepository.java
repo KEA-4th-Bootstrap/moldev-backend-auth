de92214642d8c888f838a3e5f7b34e0a39789fc4
kdhhuns2000 <52370750+kdhhuns2000@users.noreply.github.com>
2024-04-15 09:15:12 +0900

    KBB-28 [Feat] existsByEmail 추가

--- src/main/java/org/bootstrap/auth/repository/MemberRepository.java
+++ src/main/java/org/bootstrap/auth/repository/MemberRepository.java
@@ -2,9 +2,13 @@ package org.bootstrap.auth.repository;
 
 import org.bootstrap.auth.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
+
+    Boolean existsByEmail(String email);
 }
