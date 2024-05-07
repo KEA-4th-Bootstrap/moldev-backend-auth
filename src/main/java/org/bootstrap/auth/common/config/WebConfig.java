5728e4491672f46d1b661fcf135143f23d5825a4
kdhhuns2000 <52370750+kdhhuns2000@users.noreply.github.com>
2024-05-08 04:45:09 +0900

    KBB-66 [Feat] cors 설정 추가

---
+++ src/main/java/org/bootstrap/auth/common/config/WebConfig.java
@@ -0,0 +1,18 @@
+package org.bootstrap.auth.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("Authorization", "Content-Type")
+                .allowCredentials(true);
+    }
+}
