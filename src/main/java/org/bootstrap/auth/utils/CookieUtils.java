e9f549f05db9076a460c0f3d5deed5bd392f8dc8
RyuKwanKon <rhks33rhs@gmail.com>
2024-05-30 15:13:43 +0900

    [fix] cookie 생성 객체 변경

--- src/main/java/org/bootstrap/auth/utils/CookieUtils.java
+++ src/main/java/org/bootstrap/auth/utils/CookieUtils.java
@@ -2,13 +2,24 @@ package org.bootstrap.auth.utils;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseCookie;
 
 public class CookieUtils {
     public static void addCookie(HttpServletResponse response, String name, String value) {
-        Cookie cookie = new Cookie(name, value);
-        cookie.setPath("/");
-        cookie.setHttpOnly(true);
-        response.addCookie(cookie);
+//        Cookie cookie = new Cookie(name, value);
+//        cookie.setPath("/");
+//        cookie.setHttpOnly(true);
+//        cookie.setSecure(true);
+//        cookie.setSameSite("none");
+
+        ResponseCookie cookie = ResponseCookie.from(name, value)
+                .path("/")
+                .sameSite("None")
+                .httpOnly(false)
+                .secure(true)
+                .build();
+
+        response.addHeader("Set-Cookie", cookie.toString());
     }
 
     public static Cookie getCookie(Cookie[] cookies, String name) {
